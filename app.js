d8c4dbbe3a885b57a83652f4d60d7e7ec5da7c8f
Guilheeeerme <guilherme_moura.10222@outlook.com>
2020-09-19 18:33:28 -0300

    Tratamento de erros básico e Morgan para logs

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const express = require("express");
 const app = express(); // Instância do express
+const morgan = require("morgan"); // Monitora as requisiçoes e mostra logs no console
 
 const rotaProdutos = require("./routes/produtos");
 const rotaPedidos = require("./routes/pedidos");
 
 const port = process.env.PORT || 3000;
 
+app.use(morgan("dev"));
+
 app.use("/produtos", rotaProdutos);
 app.use("/pedidos", rotaPedidos);
+
+// Tratamendo de erro para quando não for encotrada nenhum rota na requisição.
+app.use((req, res, next) => {
+  const erro = new Error("Não encontrado");
+  erro.status = 404;
+  next(erro);
+});
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  return res.send({ erro: { message: error.message } });
+});
 
 app.listen(port, () => {
   console.log("API Started");
--- routes/produtos.js
+++ routes/produtos.js
@@ -16,26 +16,32 @@ router
 router
   .get("/:id", (req, res) => {
     const id = req.params.id;
-    res.status(201).send({
-      message: "Método GET para buscar um produto por ID específico",
-      id,
-    });
+    if (id) {
+      res.status(201).send({
+        message: "Método GET para buscar um produto por ID específico",
+        id,
+      });
+    }
   })
   .patch("/:id", (req, res) => {
     const id = req.params.id;
-    res.status(201).send({
-      message:
-        "Método PATCH/PUT para buscar um produto por ID específico e atualizar",
-      id,
-    });
+    if (id) {
+      res.status(201).send({
+        message:
+          "Método PATCH/PUT para buscar um produto por ID específico e atualizar",
+        id,
+      });
+    }
   })
   .delete("/:id", (req, res) => {
     const id = req.params.id;
-    res.status(204).send({
-      message:
-        "Método DELETE para buscar um produto por ID específico e excluir",
-      id,
-    });
+    if (id) {
+      res.status(204).send({
+        message:
+          "Método DELETE para buscar um produto por ID específico e excluir",
+        id,
+      });
+    }
   });
 
 module.exports = router;
