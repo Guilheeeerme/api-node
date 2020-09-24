10bfb38a47116af8440c6d1f7da4ee4f8b027a1a
Guilheeeerme <guilherme_moura.10222@outlook.com>
2020-09-24 20:42:38 -0300

    CRUD pedidos

--- routes/pedidos.js
+++ routes/pedidos.js
@@ -1,43 +1,75 @@
 const express = require("express");
 const router = express.Router();
+const mysqlConnection = require("../mysql");
 
-router
-  .get("/", (req, res) => {
-    res.status(200).send({
-      message: "Método GET para buscar todos os pedidos",
-    });
-  })
-  .post("/", (req, res) => {
-    const pedido = { id_pedido: req.body.id, qtde: req.body.qtde };
+router.get("/", (req, res) => {
+  const sql = "SELECT * FROM pedidos;";
+  mysqlConnection.query(sql, (err, result) => {
+    if (err) res.status(400).send({ error: err.message });
+
+    // Ex de "Documentar" caso seja uma API pública, só um exemplo... Acrescentaria mais detalhes, detalhe sobre o tipo de dado no caso de um POST e tals
+    const response = {
+      quantidade: result.length,
+      pedidos: result.map((pedido) => {
+        return {
+          id_pedido: pedido.id_pedido,
+          id_produto: pedido.id_produto,
+          quantidade: pedido.quantidade,
+          request: {
+            tipo: "GET",
+            descricao: "Retorna detalhes do pedido",
+            url: "http://localhost:3000/pedidos/" + pedido.id_pedido,
+          },
+        };
+      }),
+    };
+    res.status(201).send(response);
+  });
+});
+
+router.post("/", (req, res) => {
+  // prettier-ignore
+  const data = { id_produto: req.body.id_produto, quantidade: req.body.quantidade, };
+
+  const sql = "INSERT INTO pedidos SET ?";
+  mysqlConnection.query(sql, data, (err, result) => {
+    if (err) res.status(400).send({ error: err.message });
+    const response = {
+      mensagem: "Pedido inserido com sucesso",
+      pedidoCriado: {
+        id_pedido: result.id_pedido,
+        id_produto: req.body.id_produto,
+        quantidade: req.body.quantidade,
+      },
+    };
+    res.status(201).send(response);
+  });
+});
+
+router.get("/:id_pedido", (req, res) => {
+  const id = req.params.id_pedido;
+  const sql = "SELECT * FROM pedidos WHERE id_pedido = ?";
+  mysqlConnection.query(sql, id, (err, pedido) => {
+    if (err) res.status(400).send({ error: err.message });
     res.status(201).send({
-      message: "Método POST para inserir um pedido",
-      pedidoCriado: pedido,
+      message: "Pedido buscado por ID",
+      pedido,
     });
   });
+});
 
-router
-  .get("/:id_pedido", (req, res) => {
-    const id = req.params.id_pedido;
-    res.status(201).send({
-      message: "Método GET para buscar um pedido por ID específico",
-      id,
-    });
-  })
-  .patch("/:id_pedido", (req, res) => {
-    const id = req.params.id_pedido;
-    res.status(201).send({
-      message:
-        "Método PATCH/PUT para buscar um pedido por ID específico e atualizar",
-      id,
-    });
-  })
-  .delete("/:id_pedido", (req, res) => {
-    const id = req.params.id;
-    res.status(204).send({
-      message:
-        "Método DELETE para buscar um pedido por ID específico e excluir",
-      id,
-    });
-  });
+router.delete("/:id_pedido", (req, res) => {
+  mysqlConnection.query(
+    "DELETE FROM pedidos WHERE id_pedido = ?",
+    [req.params.id_pedido],
+    (err, produto, fields) => {
+      if (!err) {
+        res.status(202).send({ message: "Pedido deletado com sucesso" });
+      } else {
+        res.status(400).send({ error: err.message });
+      }
+    }
+  );
+});
 
 module.exports = router;
--- routes/produtos.js
+++ routes/produtos.js
@@ -6,10 +6,24 @@ router.get("/", (req, res) => {
   const sql = "SELECT * FROM produtos;";
   mysqlConnection.query(sql, (err, produtos) => {
     if (err) res.status(400).send({ error: err.message });
-    res.status(201).send({
-      message: "Lista de todos os produtos",
-      produtos,
-    });
+
+    // Ex de "Documentar" caso seja uma API pública, só um exemplo... Acrescentaria mais detalhes, detalhe sobre o tipo de dado no caso de um POST e tals
+    const response = {
+      quantidade: produtos.length,
+      produtos: produtos.map((prod) => {
+        return {
+          id_produto: prod.id_produto,
+          nome: prod.nome,
+          preco: prod.preco,
+          request: {
+            tipo: "GET",
+            descricao: "Retorna todos os produtos",
+            url: "http://localhost:3000/produtos/" + prod.id_produto,
+          },
+        };
+      }),
+    };
+    res.status(201).send(response);
   });
 });
 
@@ -42,7 +56,7 @@ router
     const { nome, preco } = req.body;
 
     //prettier-ignore
-    const sql = "UPDATE produtos SET nome='"+nome+"', preco='"+preco+"' WHERE id_produto = '"+id+"'"; // mariadb syntax
+    const sql = "UPDATE produtos SET nome='"+nome+"', preco='"+preco+"' WHERE id_produto = '"+id+"'"; // mariadb syntax, com template string deu erro
 
     mysqlConnection.query(sql, (err, produto) => {
       if (err) res.status(400).send({ error: err.message });
